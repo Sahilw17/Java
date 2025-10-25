512d7aedf1d163b9d738b61725dbe4b907dd492d
Sahilw17 <sahilwaghole1784@gmail.com>
2025-10-25 12:37:42 +0530

    Exception Handling

---
+++ Exception Hnadling/Test.java
@@ -0,0 +1,16 @@
+class Test 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=0;
+		
+		try{
+		int c=a/b;
+		}
+		catch(ArithmeticException e){
+		System.out.println(e);
+		System.out.println("The C cannot divide by 0");
+		}
+	}
+}
