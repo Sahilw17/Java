a8acd68b26fedbbada6b0b49a5144a183876ab7e
Sahilw17 <sahilwaghole1784@gmail.com>
2025-11-05 23:53:09 +0530

    File Handling

---
+++ FileHandling/ReadWrite.java
@@ -0,0 +1,25 @@
+import java.io.*;
+class ReadWrite 
+{
+	public static void main(String[] args) 
+	{
+		//System.out.println("Hello World!");
+		try{
+			FileWriter fw =new FileWriter("sample.txt");
+			fw.write("Hello My name is Raze");
+			fw.write("My abilities are to bom,use sachlates and booom");
+			fw.close();
+			
+			FileReader fr= new FileReader("sample.txt");
+			int i;
+			while((i=fr.read()) != -1){
+				System.out.print((char)i);
+			}
+			fr.close();
+		}
+		catch(IOException e){
+			System.out.println(e.getMessage());
+		}
+		
+	}
+}
